c2d1d3322822383b99f85e777f8120146d8243c6
Christian Martinez and Chance Magno <christian@>
2017-05-23 09:40:45 -0700

    add custom array adapter

--- app/src/main/java/com/example/guest/myrestaurants/MyRestaurantsArrayAdapter.java
+++ app/src/main/java/com/example/guest/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -1,3 +1,5 @@
+package com.example.guest.myrestaurants;
+
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
@@ -11,5 +13,17 @@ public class MyRestaurantsArrayAdapter extends ArrayAdapter {
         this.mContext = mContext;
         this.mRestaurants = mRestaurants;
         this.mCuisines = mCuisines;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.length;
     }
 }
