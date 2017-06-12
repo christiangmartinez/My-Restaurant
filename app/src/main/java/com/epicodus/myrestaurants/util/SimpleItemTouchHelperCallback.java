42595dafc585261c3034d9196424aa443a9f6f8f
Guest User <Guest@Epicodus-6D.local>
2017-06-12 14:48:17 -0700

    add animations

--- app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
@@ -41,4 +41,30 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int i) {
         mAdapter.onItemDismiss(viewHolder.getAdapterPosition());
     }
+
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+
+        //  This conditional ensures we only change appearance of active items:
+
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            if (viewHolder instanceof ItemTouchHelperViewHolder) {
+
+                //  This tells the viewHolder that an item is being moved or dragged:
+
+                ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+                itemViewHolder.onItemSelected();
+            }
+        }
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+        if (viewHolder instanceof ItemTouchHelperViewHolder) {
+            ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+            itemViewHolder.onItemClear();
+        }
+    }
 }
